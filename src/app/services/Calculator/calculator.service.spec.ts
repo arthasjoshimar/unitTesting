7114e2f4a94c04069908aa46bc2a64e3fcaabbe9
DESARROLLADOR 1 WINDOWS <developerruat1@gmail.com>
2023-06-01 00:36:10 -0400

    Se crea spyon para verificar las llamadas o mock al servicio, es un spyon basico

--- src/app/services/Calculator/calculator.service.spec.ts
+++ src/app/services/Calculator/calculator.service.spec.ts
@@ -5,15 +5,20 @@ describe('Calculator services', () => {
   
   it('should add two numbers', () => {
     let loggerService = new LoggerService();
+    spyOn(loggerService, 'log');
+    // spyOn(loggerService, 'log').and.callThrough();
     const calculator = new CalculatorService(loggerService);
     let result = calculator.add(2, 2);
     expect(result).toBe(4);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', () => {
     let loggerService = new LoggerService();
+    spyOn(loggerService, 'log');
     let calculator = new CalculatorService(loggerService);
     let result = calculator.subtract(2,2);
     expect(result).toBe(0);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
 });
--- src/app/services/Logger/logger.service.ts
+++ src/app/services/Logger/logger.service.ts
@@ -5,8 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class LoggerService {
   messages: string[] = [];
-  
+
   log(message: string) {
+    debugger;
     this.messages.push(message);
   }
 }
