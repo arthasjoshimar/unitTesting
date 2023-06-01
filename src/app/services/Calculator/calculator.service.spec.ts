624fa9c66ae886a311861e2ad32e26d88056406e
DESARROLLADOR 1 WINDOWS <developerruat1@gmail.com>
2023-06-01 00:53:36 -0400

    Using beforeEach

--- src/app/services/Calculator/calculator.service.spec.ts
+++ src/app/services/Calculator/calculator.service.spec.ts
@@ -2,19 +2,23 @@ import { CalculatorService } from './calculator.service';
 import { LoggerService } from '../Logger/logger.service';
 
 describe('Calculator services', () => {
-  
+  let mockLoggerService: any;
+  let calculator:CalculatorService;
+
+  beforeEach(() => {
+    mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(mockLoggerService);
+  });
+
   it('should add two numbers', () => {
-    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculator = new CalculatorService(mockLoggerService);
+    
     let result = calculator.add(2, 2);
     expect(result).toBe(4);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
 
-  it('should subtract two numbers', () => {
-    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculator = new CalculatorService(mockLoggerService);
-    let result = calculator.subtract(2,2);
+  it('should subtract two numbers', () => {   
+    let result = calculator.subtract(2, 2);
     expect(result).toBe(0);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
