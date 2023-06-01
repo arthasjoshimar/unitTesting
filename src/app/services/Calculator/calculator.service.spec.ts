5569dbb860ce6c18c445344b73a86509666098b1
DESARROLLADOR 1 WINDOWS <developerruat1@gmail.com>
2023-05-31 23:47:06 -0400

    Dependency with another service, simple injection

--- src/app/services/Calculator/calculator.service.spec.ts
+++ src/app/services/Calculator/calculator.service.spec.ts
@@ -1,14 +1,18 @@
 import { CalculatorService } from './calculator.service';
+import { LoggerService } from '../Logger/logger.service';
 
 describe('Calculator services', () => {
+  
   it('should add two numbers', () => {
-    const calculator = new CalculatorService();
+    let loggerService = new LoggerService();
+    const calculator = new CalculatorService(loggerService);
     let result = calculator.add(2, 2);
     expect(result).toBe(4);
   });
-  
+
   it('should subtract two numbers', () => {
-    let calculator = new CalculatorService();
+    let loggerService = new LoggerService();
+    let calculator = new CalculatorService(loggerService);
     let result = calculator.subtract(2,2);
     expect(result).toBe(0);
   });
